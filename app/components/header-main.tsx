5030b48c7ff567a721964483a9218562130aa661
reinvent-ws <reinventws@gmail.com>
2026-02-16 02:45:32 -0300

    I installed the Font Awesome plugin and replaced the SVG with a Font Awesome icon.

--- app/components/header-main.tsx
+++ app/components/header-main.tsx
@@ -2,8 +2,11 @@ import Image from "next/image"
 import { useState } from "react";
 import { ButtonHover } from "./button-hover";
 import { menuList } from "../lists/all-lists";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleUser, faMoon, faSun } from "@fortawesome/free-regular-svg-icons";
 
 import { Michroma } from "next/font/google";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 const fontStyle = Michroma( {weight: '400', subsets: ['latin-ext']});
 
 export const HeaderMain = (() => {
@@ -13,18 +16,14 @@ export const HeaderMain = (() => {
 
     const elementButton = darkMode ?
         (
-            <div title="Dark">                
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
-                </svg>
+            <div title="Dark">
+                <FontAwesomeIcon icon={faMoon} size="lg" />
             </div>
         )
         :
         (
             <div title="Light">  
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
-                </svg>
+                <FontAwesomeIcon icon={faSun} size="lg"/>
             </div>
         )
 
@@ -42,10 +41,11 @@ export const HeaderMain = (() => {
                     <p className='tracking-[10.5px] max-[859px]:hidden'>web studio</p>
                 </div>
             </div>
-            <div className="flex gap-6 pr-4 text-black">
-                <div className={`flex items-center gap-10 pr-2 tracking-widest`}>
+            <div className="flex items-center gap-6 pr-4 text-black">
+                <div className={`flex items-center gap-10 pr-2 tracking-widest max-lg:hidden`}>
                     {menus.map((menu: any, idx: number)=>(<p key={idx}>{menu}</p>))}
                 </div>
+                <FontAwesomeIcon icon={faBars} className="min-[1024px]:invisible"/>
                 <div className='flex items-center gap-6 pl-8 border-l-[1px] border-black text-black/50'>
                     <ButtonHover
                         sendDM={darkMode}
@@ -54,9 +54,9 @@ export const HeaderMain = (() => {
                     />
                     <ButtonHover
                         children={
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                            </svg>
+                            <div title='User'>
+                                <FontAwesomeIcon icon={faCircleUser} size="lg" />
+                            </div>
                         }
                     />
                 </div>
